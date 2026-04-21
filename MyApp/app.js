f6a7bb8575dc6e05180f4e04c11667755725538a
masudzzz <abdullahalmasud6590@gmail.com>
2026-04-21 23:56:05 +0600

    updated website + new features

--- MyApp/app.js
+++ MyApp/app.js
@@ -1,10 +1,34 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Masud! Docker App Running 🚀');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Contact form submit
+app.post("/contact", (req, res) => {
+  const data = req.body;
+
+  console.log("New Contact Request:", data);
+
+  let oldData = [];
+  if (fs.existsSync("data.json")) {
+    oldData = JSON.parse(fs.readFileSync("data.json"));
+  }
+
+  oldData.push(data);
+
+  fs.writeFileSync("data.json", JSON.stringify(oldData, null, 2));
+
+  res.send("Thanks! I will contact you soon.");
 });
 
 app.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log("Personal website running on port 3000");
 });
